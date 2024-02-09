e750bb6950dc11e070114ab205714b0b3f7752ff
Lochan <98405764+lochanchugh@users.noreply.github.com>
2024-02-09 14:41:41 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,1 +1,13 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Blehcoin is ERC20 {
+    constructor() ERC20("Bleh", "LOCHAN") {
+        _mint(msg.sender, 10000*(10**18));
+        }
+}
+
 
