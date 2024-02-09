9b2ed907c7374b1b01791a48580758bdf21451e1
Lochan <98405764+lochanchugh@users.noreply.github.com>
2024-02-09 14:17:58 +0530

    Update hello.sol

--- hello.sol
+++ hello.sol
@@ -3,24 +3,4 @@
 pragma solidity >=0.7.0 <0.9.0;
 contract hello {
     string hey = "Hello bro!";
-    int i=10;
-    uint j=120;
-    function display (int i) public view returns(int){
-        return i;
-    }
-    function addition (int x, int y) public view returns (int){
-        return x+y;
-    }
-    function substaraction (int x, int y) public view returns (int){
-        return x-y;
-    }
-    function division (int x, int y) public view returns (int){
-        if (y!=0){
-            return x/y;
-        }
-        
-    }
-    function multiplication (int x, int y) public view returns (int){
-        return x*y;
-    }
 }
