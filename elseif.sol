d35e31cd309c40b0d6167ec23edaf8617e68348c
Lochan <98405764+lochanchugh@users.noreply.github.com>
2024-02-09 14:26:20 +0530

    Create elseif.sol

---
+++ elseif.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ifelse{
+    function checkoddeven(int num) public view returns (string){
+        if (num%2==0) {
+            returns 'even';
+        } 
+        else {
+            returns 'odd';
+        }
+    }
+}
